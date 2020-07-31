1a024d202b1ccc8e0dc65777afa460d0da045321
mudziok <mudziok@gmail.com>
2020-07-31 17:50:43 +0200

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Sticker Creator</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+body {
+  background: #64dbd1;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  text-align: center;
+  color: rgb(240, 255, 252);
+  margin-top: 60px;
+}
+
+h1 {
+  font-size: 56px;
+}
+</style>
