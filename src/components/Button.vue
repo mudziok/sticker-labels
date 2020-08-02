9abcace50786a55b78d0f49a8677f3d5cb99448f
mudziok <mudziok@gmail.com>
2020-08-02 23:41:21 +0200

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="button">
+    <div class="body" ontouchstart="">
+      <slot />
+    </div>
+    <div class="shadow"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    name,
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  margin: 0;
+  position: relative;
+}
+
+.body {
+  padding: 0.75em 1.5em;
+
+  background: #c6e9e5;
+  color: var(--accent);
+  border-radius: 20px;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+
+  user-select: none; 
+}
+
+.body:active {
+  transform: translateY(6px);
+}
+
+.shadow {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 40px;
+  z-index: -2;
+
+  background: var(--accent);
+  border-radius: 0 0 20px 20px;
+  transform: translateY(6px);
+}
+</style>
